130bdd2de6f078bad3ab999a5d0c40498df851de
Theo <theo.reichert7@gmail.com>
2019-03-22 00:23:21 +0100

    2.0

--- picture sorting.py
+++ picture sorting.py
@@ -1,39 +1,53 @@
 import os
 import datetime
 import shutil
+import pathlib
 
 path = "C:\\Users\\Emil\\Desktop\\picture sorting\\"
-os.chdir(path)
-print(os.getcwd())
+
+mon = {
+    "01" : "Januar",
+    "02" : "Februar",
+    "03" : "März",
+    "04" : "April",
+    "05" : "Mai",
+    "06" : "Juni",
+    "07" : "Juli",
+    "08" : "August",
+    "09" : "September",
+    "10" : "Oktober",
+    "11" : "November",
+    "12" : "Dezember"
+}
 
 for dirpath, dirnames, filenames in os.walk(path):
-    for e in filenames:
-        picture = str(e)
-        picobj = open(picture, "rb")
-        f = picobj.read(761)
-        t = str(f)
-        try:
-            pos1 = t.find("Digital Camera FinePix XP30")
-            # print(pos1)
-            x = pos1 + 62
-            excerpt = t[pos1:x]
-            # print(excerpt)
-            y = excerpt.split("\\x00")
+    for f in filenames:
+        if f.find(".JPG") != -1:
+            print(f)
+            picobj = open(f,"rb")
+            header = str(picobj.read(761))
+            # print(header)
+            pos = header.find("Digital Camera FinePix XP30 Ver1.01") + 39
+            # print(pos)
+            x = pos + 10
+            # print(x)
+            y = header[pos:x]
             # print(y)
-            # print(y[1])
-            z = y[1].split(":")
-            # print(z)
-            year = z[0]
-            if os.path.isdir(str(dirpath) + year) == True:
-                shutil.move(str(dirpath) + str(e), str(dirpath) + str(year))
-            elif os.path.isdir(str(dirpath) + year) == False:    
-                os.mkdir(str(dirpath) + year)
-                shutil.move(str(dirpath) + str(e), str(dirpath) + str(year))
-        except:
-            print("failed")
-
-# count = 0
-# l = "Digital Camera FinePix XP30 Ver1.01\\x002014:07:31 17:46:30\\x00"
-# for e in l:
-#     count = count + 1
-# print(count)
+            z = y.split(":")
+            year = str(z[0])
+            month = str(z[1])
+            day = str(z[2])
+            date = year + "\\" + mon[month] + "\\" + day
+            print(date)
+            source = str(dirpath) + str(f)
+            destination = "C:\\Users\\Emil\\Desktop\\sorted\\" + date + "\\"
+            print(source)
+            print(destination)
+            if os.path.isdir(destination) == True:
+                shutil.move(source, destination)
+            elif os.path.isdir(destination) == False:
+                os.makedirs(destination)
+                shutil.move(source, destination)
+            pass
+        else:
+            pass
